a47d7cee15c7c01d6e25fe7ab780df6ca8ca0063
hamso <hamsonassor14@gmail.com>
2021-10-04 12:20:33 +0300

    added use of routers logic to the urls and added path for it

--- django_api/urls.py
+++ django_api/urls.py
@@ -14,13 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from test_app.views import SimpleGenerics, SimpleGenericsUpdate #,Simple
+from django.urls import path, include
+from test_app.views import SimpleViewset #SimpleGenerics, #SimpleGenericsUpdate, #,Simple
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("simple-viewset", SimpleViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('simple/', Simple.as_view()),
     # path('simple/<int:id>', Simple.as_view()),
-    path('simple-generics', SimpleGenerics.as_view()),
-    path('simple-generics/<int:id>', SimpleGenericsUpdate.as_view()) #with <int:id> an error of pk is returned therefore need for lookup_field, but <int:pk> works perfectly without lookup_field
+    # path('simple-generics', SimpleGenerics.as_view()),
+    # path('simple-generics/<int:id>', SimpleGenericsUpdate.as_view()), #with <int:id> an error of pk is returned therefore need for lookup_field, but <int:pk> works perfectly without lookup_field
+    path("", include(router.urls)),
 ]
