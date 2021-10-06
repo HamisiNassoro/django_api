7159597d8929c3957f1a6c21f8d8d1393aa90399
hamso <hamsonassor14@gmail.com>
2021-10-06 16:37:23 +0300

    registered the Blog model to the admin and updated the requirements.txt

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
+from test_app.models import Blog
+
+admin.site.register((Blog))
+
+
+
+
+
+
+
+
 # from .models import TestModel, ModelX, ModelY
 
 # # Register your models here.
