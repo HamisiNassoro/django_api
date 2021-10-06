8630329a46548d7c40390c9333a83069e0788b76
hamso <hamsonassor14@gmail.com>
2021-10-06 16:36:12 +0300

    started new model Blog and run migrations

--- test_app/models.py
+++ test_app/models.py
@@ -1,6 +1,46 @@
 from django.db import models
-from django.db.models.fields import BigIntegerField, BooleanField, FloatField, IntegerField, PositiveIntegerField, TextField
-from django.db.models.fields.related import ForeignKey, ManyToManyField, OneToOneField
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    contents = models.TextField()
+    author = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.db.models.fields import BigIntegerField, BooleanField, FloatField, IntegerField, PositiveIntegerField, TextField
+# from django.db.models.fields.related import ForeignKey, ManyToManyField, OneToOneField
 
 
         # models.CharField(max_length=200)
