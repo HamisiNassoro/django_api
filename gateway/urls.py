aefe111a0f9368e2f37e12a0571824a94c6c078d
hamso <hamsonassor14@gmail.com>
2021-10-14 11:21:10 +0300

    imported RegisterView and added the register path endpoint

--- gateway/urls.py
+++ gateway/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from gateway.views import LoginView
+from gateway.views import LoginView,RegisterView
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
 ]
