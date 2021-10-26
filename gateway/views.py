f6ce3d63f46b2cce65cf83d1398bc823810736b3
hamso <hamsonassor14@gmail.com>
2021-10-26 20:39:56 +0300

    created refresh view and fumction to verify token

--- gateway/views.py
+++ gateway/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 import random
 import string
 from rest_framework.views import APIView
-from gateway.serializers import LoginSerializer,RegisterSerializer
+from gateway.serializers import LoginSerializer,RegisterSerializer, RefreshSerializer
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 
@@ -43,11 +43,14 @@ class LoginView(APIView):
         if not user:
             return Response({"error": "Invalid email or password"}, status=400)
 
+        Jwt.objects.filter(user_id=user.id).delete() #ensures token is deleted after session and a new one generated after new login
+
         access = get_access_token({'user_id': user.id})
         refresh = get_refresh_token()
 
         Jwt.objects.create(
-            user_id=user.id, access=access, refresh=refresh
+            # user_id=user.id, access=access.decode(), refresh=refresh.decode() #this how should be in tutorial to remove the b' before token on admin
+            user_id=user.id, access=access, refresh=refresh #works well on my machine without the b'
         )
 
         return Response({"access": access, "refresh": refresh})
@@ -62,3 +65,42 @@ class RegisterView(APIView):
         CustomUser.objects._create_user(**serializer.validated_data)
 
         return Response({"success": "User created."})
+
+def verify_token(token):
+    # decode the token
+    try:
+        decoded_data = jwt.decode(token, settings.SECRET_KEY, algorithm="HS256")
+    except Exception:
+        return None
+
+    # check if token has expired
+    exp = decoded_data["exp"]
+
+    if datetime.now().timestamp() > exp:
+        return None
+    return decoded_data
+
+class RefreshView(APIView):
+    serializer_class = RefreshSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data = request.data)
+        serializer.is_valid(raise_exception = True)
+
+        try:
+            active_jwt = Jwt.objects.get(refresh=serializer.validated_data["refresh"])
+        except Jwt.DoesNotExist:
+            return Response({"error": "refresh token not found"}, status = "400")
+
+        if not verify_token(serializer.validated_data["refresh"]):
+            return Response({"error": "Token is invalid or has expired"})
+
+        access = get_access_token({'user_id': active_jwt.user.id})
+        refresh = get_refresh_token()
+
+        active_jwt.access = access.decode()
+        active_jwt.refresh = refresh.decode()
+        active_jwt.save()
+
+        return Response({"access": access, "refresh": refresh})
+
