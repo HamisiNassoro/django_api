a0014570e2d1ad5c0ca7bbdba702738721fa078d
hamso <hamsonassor14@gmail.com>
2021-11-24 16:47:32 +0300

    updated user urls

---
+++ user/urls.py
@@ -0,0 +1,13 @@
+from re import DEBUG
+from rest_framework import urlpatterns
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from user.views import CustomUserView, UserProfileView
+
+router = DefaultRouter()
+router.register("user", CustomUserView)
+router.register("user-profile", UserProfileView)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
