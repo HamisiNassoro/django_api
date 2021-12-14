f983c26bd489cd49c0297a7063bfb5c75a557a20
hamso <hamsonassor14@gmail.com>
2021-12-14 11:32:41 +0300

    registered moerls for event_controller to admin

--- event_controller/admin.py
+++ event_controller/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from event_controller.models import EventMain, EventFeature, EventAttendant
 
 # Register your models here.
+admin.site.register(EventMain)
+admin.site.register(EventFeature)
+admin.site.register(EventAttendant)
