be3346a833bec9f5d302f4d86aac366829c925e5
hamso <hamsonassor14@gmail.com>
2022-01-14 11:12:17 +0300

    created celery file and setup

---
+++ django_api/celery.py
@@ -0,0 +1,8 @@
+from celery import Celery
+import os 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_api.settings')
+
+app = Celery('djang_api')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
