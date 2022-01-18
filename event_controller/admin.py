f0e2b627cec91af15647cb4f6569f57ba1e81f04
hamso <hamsonassor14@gmail.com>
2022-01-18 13:51:51 +0300

    registered the dog model in the admin file

--- event_controller/admin.py
+++ event_controller/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from event_controller.models import EventMain, EventFeature, EventAttendant
+from event_controller.models import EventMain, EventFeature, EventAttendant, Dog
 
 # Register your models here.
 admin.site.register(EventMain)
 admin.site.register(EventFeature)
 admin.site.register(EventAttendant)
+admin.site.register(Dog)
