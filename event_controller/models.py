208b32275f9c5c463ad9e5e85b4f8b6b4fcb061c
hamso <hamsonassor14@gmail.com>
2022-01-18 13:02:04 +0300

    create model Dog and made migration

--- event_controller/models.py
+++ event_controller/models.py
@@ -41,3 +41,10 @@ class EventAttendant(models.Model):
 
     def __str__(self):
         return f"{self.eventmain.title} - {self.user.name}"
+
+class Dog(models.Model):
+    url = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.url
