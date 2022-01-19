dcd86fce91cc27e519abe75c841e7200b7946ebc
hamso <hamsonassor14@gmail.com>
2022-01-19 10:34:54 +0300

    consumed an api that updates a dog picture and saves on the dog model

--- django_api/tasks.py
+++ django_api/tasks.py
@@ -1,7 +1,26 @@
 from celery import app
+# from celery import task
 import time
+import requests
+from event_controller.models import Dog
 
 @app.shared_task()
 def test():
     time.sleep(5)          #sleeps for 5 secs
     print("Hello Async")
+
+@app.shared_task()
+def populate_dog():
+    url = 'https://dog.ceo/api/breeds/image/random'
+
+    try:
+        res = requests.get(url)
+    except requests.ConnectionError as e:
+        raise Exception("Failed Operation", e)
+
+    if res.status_code in [200, 201]:
+        #create dog entry
+        # print(res.json())
+        data = res.json()
+        image_url = data.get("message", "")
+        Dog.objects.create(url=image_url)
