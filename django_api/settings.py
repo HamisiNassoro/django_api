7f43ca139459824d001b1ca25e69c58b861a733e
hamso <hamsonassor14@gmail.com>
2022-01-21 16:36:19 +0300

    added Email configurations on settings

--- django_api/settings.py
+++ django_api/settings.py
@@ -104,6 +104,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_api.wsgi.application'
 
 
+# EmailConfiguration
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
