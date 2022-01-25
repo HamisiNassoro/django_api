0c69c588389c65f34ca6dac810931b05d8848f77
hamso <hamsonassor14@gmail.com>
2022-01-25 16:55:27 +0300

    modified migrations

--- user/migrations/0004_customuser_mobile.py
+++ user/migrations/0004_customuser_mobile.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='customuser',
             name='mobile',
-            field=models.IntegerField(default=''),
+            field=models.IntegerField(default=254),
             preserve_default=False,
         ),
     ]
