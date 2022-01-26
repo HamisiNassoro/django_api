40cbd9f73014c384a598f7b580d5f8c771007e16
hamso <hamsonassor14@gmail.com>
2022-01-26 19:20:07 +0300

    removed otp fields on serializer and its views

--- user/serializers.py
+++ user/serializers.py
@@ -21,7 +21,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ("email", "name", "mobile", "isVerified", "user_profile", "created_at", "updated_at")
+        fields = ("email", "name", "user_profile", "created_at", "updated_at")
 
 
 
